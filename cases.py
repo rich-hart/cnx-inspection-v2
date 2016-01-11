e404cdde96d60150ab00b2b677a191404528ec2a
rich-hart <rich.hart@rice.edu>
2016-01-10 22:17:50 -0600

    improved load tests and sample cases

--- cases.py
+++ cases.py
@@ -2,6 +2,7 @@ import frameworks
 import cv2
 import cv
 import numpy
+import unittest
 class DefaultTest(frameworks.PNGs):
     def equality(self):
         equal = numpy.array_equal(self.image_i, self.image_j)
@@ -14,9 +15,10 @@ class MyTest1(frameworks.PNGs):
 
         gray_j = cv2.cvtColor(self.image_j,cv2.COLOR_BGR2GRAY)
         hist_j = cv2.calcHist([gray_j],[0],None,[256],[0,256])
-        measure = cv.CompareHist(hist_i, hist_j, cv.CV_COMP_CORREL) 
 
-        self.assertGreater(measure,.5)
+        measure = cv2.compareHist(hist_i, hist_j, cv.CV_COMP_CORREL) 
+        self.assertGreater(measure,.9)
+
     def equality(self):
         if self.page_i==0 or self.page_j ==0:
             raise unittest.SkipTest("Don't run this test on the first pages")
@@ -25,6 +27,13 @@ class MyTest1(frameworks.PNGs):
 
 class MyTest2(frameworks.PNGs):
     def histogram_cmp_bhatta(self):
-        self.threshold = .2
+        self.threshold = .9
+        gray_i = cv2.cvtColor(self.image_i,cv2.COLOR_BGR2GRAY)
+        hist_i = cv2.calcHist([gray_i],[0],None,[256],[0,256])
 
+        gray_j = cv2.cvtColor(self.image_j,cv2.COLOR_BGR2GRAY)
+        hist_j = cv2.calcHist([gray_j],[0],None,[256],[0,256])
+
+        self.measure = cv2.compareHist(hist_i, hist_j, cv.CV_COMP_BHATTACHARYYA) 
+        self.assertGreater(self.measure,self.threshhold)
 
--- utils.py
+++ utils.py
@@ -1,16 +1,31 @@
 import unittest
 from cases import DefaultTest
 from importlib import import_module
-
-
+import psycopg2
+import inspect
 def test_generator(settings):
     test_cases = unittest.TestSuite()
     cases = import_module(settings['cases'])
+
+    with psycopg2.connect(database=settings['database']) as con:
+        with con.cursor() as cur:
+            cur.execute("SELECT COUNT(Page) FROM png_a")
+            total_M= cur.fetchone()[0]
+            cur.execute("SELECT COUNT(Page) FROM png_b")
+            total_N= cur.fetchone()[0] 
     for case_name in settings['include']:
         TestClass = cases.__getattribute__(case_name)
         setattr(TestClass,'_settings',settings)
-        for p1, p2 in [(1, 2), (3, 4)]:
-            test_cases.addTest(TestClass('equality', p1, p2))
+        SuperClass = inspect.getmro(TestClass)[1]
+
+        method_list = inspect.getmembers(TestClass, predicate=inspect.ismethod)
+        super_method_list = inspect.getmembers(SuperClass,predicate=inspect.ismethod)
+        test_method_list = list(set(method_list)-set(super_method_list))
+        test_name_list = [ method[0] for method in test_method_list if method[0]!='tearDownClass' and method[0]!='setUpClass']
+        for test_name in test_name_list:
+            for pi in range(0,total_M):
+                for pj in range(0,total_N):
+                    test_cases.addTest(TestClass(test_name, pi, pj))
     def load_tests(loader, tests, pattern):
     # FIXME: declare me as a decorator
         return test_cases
