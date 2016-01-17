ecf98eaa944a94e8ab32ae9e47fa3af0c22f6c27
rich-hart <rich.hart@rice.edu>
2016-01-17 01:01:00 +0000

    test update

--- test.py
+++ test.py
@@ -109,6 +109,34 @@ class Core(unittest.TestCase):
         result = self.target(load,run)
         self.assertEqual(expect,result)
 
+    def test_color_change(self):
+        load = "python loaddb.py data/test/A.pdf data/test/E.pdf"
+        run = "python inspection.py"
+        expect = [ (1,1),
+                   (3,3),
+                   (4,4),
+                   (6,6),
+                   (7,7),
+                   (9,9), 
+                   (10,10), ]
+        result = self.target(load,run)
+        self.assertEqual(expect,result)
+
+        load = "python loaddb.py data/test/A.pdf data/test/E.pdf"
+        run = "python inspection.py --include MyTest1 --check any"
+        expect = [ (1,1),
+                   (2,2),
+                   (3,3),
+                   (4,4),
+                   (5,5),
+                   (6,6),
+                   (7,7),
+                   (8,8),
+                   (9,9), 
+                   (10,10), ]
+        result = self.target(load,run)
+        self.assertEqual(expect,result)
+
 if __name__ == '__main__':
     unittest.main()
 
