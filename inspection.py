63da3106f47cc4a9f4f4b14058187d010b10dd7f
rich-hart <rich.hart@rice.edu>
2016-01-26 16:37:35 +0000

    adding script help messages

--- inspection.py
+++ inspection.py
@@ -16,12 +16,12 @@ def run(settings):
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--include', action='append', default=['DefaultTest'])
-    parser.add_argument('--exclude', action='append', default=[])
-    parser.add_argument('--output', type=str, default='output.log')
-    parser.add_argument('--results', type=str, default='results.log')
-    parser.add_argument('--cases', type=str, default='cases')
-    parser.add_argument('--check', type=str, choices=['any','all'],default='all')
+    parser.add_argument('--include', action='append', default=['DefaultTest'], help="Include additional test classes (default=[DefaultTest])")
+    parser.add_argument('--exclude', action='append', default=[], help="Exclude test classes.")
+    parser.add_argument('--output', type=str, default='output.log', help="Test execution output file (default=output.log).")
+    parser.add_argument('--results', type=str, default='results.log', help="Test results output file, each line is a python dictionary (default=results.log).")
+    parser.add_argument('--cases', type=str, default='cases', help="Python module which stores test cases (default=cases).")
+    parser.add_argument('--check', type=str, choices=['any','all'],default='all', help="Require that any/all test cases pass for pages to be related (default=all).")
     parser.add_argument('pdf_a', type=str)
     parser.add_argument('pdf_b', type=str) 
 
